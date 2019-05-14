b67a6428c4442221321a75f84b5eec67c231880f
mojerro <mojerro@163.com>
2019-05-14 20:23:23 +0800

    主页框架

---
+++ pages/article.vue
@@ -0,0 +1,21 @@
+<style lang="scss"></style>
+
+<template>
+  <div class="container">
+    this is article
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  async created() {},
+  async mounted() {},
+  methods: {}
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,92 @@
+<style lang="scss">
+.container {
+  align-items: center;
+
+  .carousel {
+    max-width: 1000px;
+  }
+  .content {
+    padding: 5rem;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+
+    .box-card {
+      width: 600px;
+      margin: 15px 20px;
+
+      .image {
+        width: 100%;
+        display: block;
+      }
+    }
+  }
+}
+</style>
+
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        qnhszm
-      </h1>
-      <h2 class="subtitle">
-        My spectacular Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
+  <div class="container">
+    <el-row type="flex" justify="center">
+      <el-col :span="16">
+        <el-carousel class="carousel" height="350px">
+          <el-carousel-item v-for="item in carousels" :key="item">
+            <el-image
+              :src="item"
+              fit="contain"
+              style="height: 350px; width: auto;"
+            ></el-image>
+          </el-carousel-item>
+        </el-carousel>
+      </el-col>
+    </el-row>
+    <div class="content">
+      <!-- card start -->
+      <el-card
+        v-for="i in arr(10)"
+        :key="i"
+        class="box-card"
+        :body-style="{ padding: '0px' }"
+      >
+        <el-image
+          style="width: 600px; height: 300px;"
+          src="http://mmbiz.qpic.cn/mmbiz_jpg/6NfezMJMwPXhfW79PJ1tJ3kIo4XUK3UvLiaQLXTtLsEGpMAicibopbqgMLIua10sicOCyWMype8VZib5AvuAsERcX0w/0"
+          alt="图片"
+          fit="cover"
+          class="image"
+        />
+        <div style="padding: 14px;">
+          <span>好吃的汉堡</span>
+        </div>
+      </el-card>
+      <!-- card end -->
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import carousel1 from '~/static/carousel1.jpg'
+import carousel2 from '~/static/carousel2.jpg'
 
 export default {
-  components: {
-    Logo
+  components: {},
+  data() {
+    return {
+      carousels: [carousel1, carousel2]
+    }
+  },
+  computed: {},
+  watch: {},
+  async created() {},
+  async mounted() {},
+  methods: {
+    arr: function ls(count) {
+      return Array(++count)
+        .join('0')
+        .split('')
+        .map(function(v, i) {
+          return ++i
+        })
+    }
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
